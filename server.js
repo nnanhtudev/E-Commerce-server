ae5c21d9b31968d74a56955315e2f3d2f986a456
nnanhtudev <nnanhtu.dev@gmail.com>
2023-12-24 14:08:19 +0700

    setup sequelize-cli & connect DB

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ import http from "http";
 import app from "./src/app";
 import "dotenv/config";
 import config from "./src/config/index";
+import connectDB from "./src/config/connectDB";
 
 const server = http.createServer(app);
+
+//connectDB
+connectDB();
 
 server.listen(config.PORT, () => {
   console.log(`Server listening on http://localhost:${config.PORT}`);
---
+++ src/config/connectDB.js
@@ -0,0 +1,27 @@
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+// Option 3: Passing parameters separately (other dialects)
+const database = process.env.DB_NAME;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const dialect = process.env.DB_DIALECT;
+
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  port,
+  dialect,
+  /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+export default connectDB;
